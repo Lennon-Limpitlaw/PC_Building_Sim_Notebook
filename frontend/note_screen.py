a272c15f99892a8ee5e6ee0b7ac7fb9fc15c096a
Lennon Limpitlaw <ljthomas01@icloud.com>
2020-04-24 19:39:57 +0100

    Started working on the two notes pages

---
+++ frontend/note_screen.py
@@ -0,0 +1,8 @@
+from kivy.uix.screenmanager import Screen
+
+
+class NoteScreen(Screen):
+    def __init__(self, **kwargs):
+        super(NoteScreen, self).__init__(**kwargs)
+
+    
--- frontend/notes_screen.py
+++ frontend/notes_screen.py
@@ -1,6 +1,6 @@
 from kivy.uix.screenmanager import Screen
 
 
-class HomeScreen(Screen):
+class NotesScreen(Screen):
 	def __init__(self, **kwargs):
 		super(HomeScreen, self).__init__(**kwargs)
