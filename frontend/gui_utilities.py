f6af888d50dbb704855a7ff32e2db4017a9f670c
Lennon Limpitlaw <ljthomas01@icloud.com>
2020-04-28 19:30:03 +0100

    Finished the build of the note screen. Started working on individual note screen

--- frontend/gui_utilities.py
+++ frontend/gui_utilities.py
@@ -1,4 +1,6 @@
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.checkbox import CheckBox
 
 
 class SaveButton(Button):
@@ -91,5 +93,43 @@ class BackButton(Button):
 class NewNoteButton(Button):
 	def __init__(self, x, y, text, **kwargs):
 		super(NewNoteButton, self).__init__(**kwargs)
+		self.pos_hint = {'x':x, 'y':y}
+		self.text = text
+
+
+class CustomerBar(TextInput):
+	def __init__(self, x, y, text, **kwargs):
+		super(CustomerBar, self).__init__(**kwargs)
+		self.pos_hint = {'x':x, 'y':y}
+		self.text = text
+		self.multiline = False
+
+
+class RequirementsBar(TextInput):
+	def __init__(self, x, y, text, **kwargs):
+		super(RequirementsBar, self).__init__(**kwargs)
+		self.pos_hint = {'x':x, 'y':y}
+		self.text = text
+		self.multiline = True
+
+
+class DeadlineBar(TextInput):
+	def __init__(self, x, y, text, **kwargs):
+		super(DeadlineBar, self).__init__(**kwargs)
+		self.pos_hint = {'x':x, 'y':y}
+		self.text = text
+		self.multiline = False
+
+
+class CompletedCheckBox(CheckBox):
+	def __init__(self, x, y, state, **kwargs):
+		super(CompletedCheckBox, self).__init__(**kwargs)
+		self.pos_hint = {'x':x, 'y':y}
+		self.active = state
+
+
+class SubmitNoteButton(Button):
+	def __init__(self, x, y, text, **kwargs):
+		super(SubmitNoteButton, self).__init__(**kwargs)
 		self.pos_hint = {'x':x, 'y':y}
 		self.text = text
--- frontend/note_screen.py
+++ frontend/note_screen.py
@@ -1,5 +1,7 @@
 from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
 from backend.database.wrapper import Handler
+from .button_utilities import CustomerBar, RequirementsBar, DeadlineBar, CompletedCheckBox, SubmitNoteButton
 
 
 class NoteScreen(Screen):
@@ -7,8 +9,15 @@ class NoteScreen(Screen):
         super(NoteScreen, self).__init__(**kwargs)
         self.__handler = Handler(filename)
 
-    def build(self, noteID):
+    def build(self, noteID, saveID):
+        self.clear_widgets()
+        self.__bars = []
+
+        if saveID is not None:
+            self.__saveID = saveID
+
         if noteID is not None:
+            self.__existing = True
             note = self.__handler.select_query('*', 'notes', ['noteID = '+str(noteID)])
             self.__noteID = noteID
             customer = note[2]
@@ -17,5 +26,35 @@ class NoteScreen(Screen):
             completed = note[4]
 
         else:
+            self.__existing = False
             notes = self.__handler.select_query('*', 'notes', None)
-            self.__saveID = len(notes) 
+            self.__noteID = len(notes)
+            customer = ''
+            requirements = ''
+            deadline = ''
+            completed = False
+
+        customer_bar = CustomerBar(0.4, 0.85, customer)
+        requirements_bar = RequirementsBar(0.4, 0.5, requirements)
+        deadline_bar = DeadlineBar(0.4, 0.35, deadline)
+        completed_checkbox = CompletedCheckBox(0.4, 0.2, completed)
+        self.__bars.append(customer_bar)
+        self.__bars.append(requirements_bar)
+        self.__bars.append(deadline_bar)
+        self.__bars.append(completed_checkbox)
+
+        for bar in self.__bars:
+            self.add_widget(bar)
+
+        self.add_widget(Label(text='Customer: ', pos_hint={'x':0.2, 'y':0.85}, size_hint=(0.2, 0.1)))
+        self.add_widget(Label(text='Requirements: ', pos_hint={'x':0.2, 'y':0.7}, size_hint=(0.2, 0.1)))
+        self.add_widget(Label(text='Deadline: ', pos_hint={'x':0.2, 'y':0.35}, size_hint=(0.2, 0.1)))
+        self.add_widget(Label(text='Completed: ', pos_hint={'x':0.2, 'y':0.2}, size_hint=(0.2, 0.1)))
+        self.add_widget(SubmitNoteButton(0.35, 0.05, 'Submit'))
+
+    def submit(self):
+        if self.__existing:
+            pass
+
+        else:
+            pass
